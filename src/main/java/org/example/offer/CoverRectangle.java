09ba8ab89a23c41f9ffe35bafaf4b78d4936c702
CalvinLin <31285778+Calvin24lky@users.noreply.github.com>
2020-07-01 01:15:12 +0800

    add solutions

---
+++ src/main/java/org/example/offer/CoverRectangle.java
@@ -0,0 +1,34 @@
+package org.example.offer;
+
+public class CoverRectangle {
+    /*
+        NO.10
+        用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+        请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+     */
+    /*
+        思路:
+            与青蛙跳台阶1类似
+     */
+
+    public static int RectCover(int n) {
+
+        if (n == 0 || n == 1 || n == 2)
+            return n;
+
+        int[] f = new int[n+1];
+        f[0] = 0;
+        f[1] = 1;
+        f[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            f[i] = f[i-1] + f[i-2];
+        }
+
+        return f[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(RectCover(0));
+    }
+}
