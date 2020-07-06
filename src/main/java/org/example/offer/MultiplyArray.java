4203b500d35510ff82b3a5de98e5fd306ff915c3
linkangying <linkangying@huya.com>
2020-07-06 13:12:44 +0800

    add files

---
+++ src/main/java/org/example/offer/MultiplyArray.java
@@ -0,0 +1,38 @@
+package org.example.offer;
+
+public class MultiplyArray {
+    /*
+        NO.51
+        给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。
+        不能使用除法。（注意：规定B[0] = A[1] * A[2] * ... * A[n-1]，B[n-1] = A[0] * A[1] * ... * A[n-2];）
+     */
+    /*
+        思路:
+            1. 双重循环 O(n^2)
+
+            2. 分析
+
+            B[0]   =    1  x  A[1] x  A[2]  x  A[3]  x ... x  A[n-1]
+            B[1]   =  A[0] x   1   x  A[2]  x  A[3]  x ... x  A[n-1]
+            B[2]   =  A[0] x  A[1] x   1    x  A[3]  x ... x  A[n-1]
+            B[3]   =  A[0] x  A[1] x  A[2]  x   1    x ... x  A[n-1]
+            ...
+            B[n-1] =  A[0] x  A[1] x  A[2]  x  A[3]  x ... x    1
+
+            所以先计算B数组的所有左边部分  然后计算B的所有右边部分
+     */
+    public int[] multiply(int[] A) {
+        int[] B = new int[A.length];
+        B[0] = 1;
+        for (int i = 1; i < B.length; i++) {
+            B[i] = B[i-1] * A[i-1];
+        }
+        int tmp = 1;
+        for (int i = A.length - 2; i >= 0; i--) {
+            tmp = tmp * A[i+1];
+            B[i] *= tmp;
+        }
+        return B;
+    }
+
+}
--- src/main/java/org/example/offer/StringToInt.java
+++ src/main/java/org/example/offer/StringToInt.java
@@ -9,21 +9,45 @@ public class StringToInt {
      */
     /*
         难点: 如何处理溢出情况
-
+        INT_MAX =  2147483647 (0x7FFFFFFF)
+        INT_MIN = -2147483648 (0x80000000)
      */
-    public int StrToInt(String str) {
+    public static int StrToInt(String str) {
         if(str == null || "".equals(str.trim()))return 0;
         char[] arr = str.trim().toCharArray();
         int i = 0;
-        boolean neg = arr[i++] == '-' ? true : false;
+//        boolean neg = arr[i++] == '-' ? true : false;
+        int flag = 1;
         int res = 0;
+        if(arr[i] == '-'){
+            flag = -1;
+        }
+        if( arr[i] == '+' || arr[i] == '-'){
+            i++;
+        }
         while (i < arr.length) {
             // 是数字
             if (arr[i] >= '0' && arr[i] <= '9') {
-                int cur = arr[i] - '0';
-                if (neg == 1 && )
+                int cur = arr[i] - '0'; // 取这个字符
+                if (flag == 1 && (res > Integer.MAX_VALUE / 10 || res == Integer.MAX_VALUE / 10 && cur > 7)) {
+                    return 0; //超过int最大值
+                }
+                if (flag == -1 && (res > Integer.MAX_VALUE / 10 || res == Integer.MAX_VALUE / 10 && cur > 8)) {
+                    return 0;
+                }
+                res = res * 10 + cur;
+                i++;
+            }
+            else {
+                // 不是数字
+                return 0;
             }
         }
+        return res * flag;
+    }
 
+    public static void main(String[] args) {
+        String str = "+123";
+        System.out.println(StrToInt(str));
     }
 }
