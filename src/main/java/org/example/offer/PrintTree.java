6649749fb0098652da48b06e3605b262e6abb638
linkangying <linkangying@huya.com>
2020-07-06 21:59:44 +0800

    add no.60

---
+++ src/main/java/org/example/offer/PrintTree.java
@@ -0,0 +1,35 @@
+package org.example.offer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PrintTree {
+    /*
+        NO.60
+        从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行。
+     */
+
+    ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (pRoot == null) {
+            return res;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(pRoot);
+        while (q.size() > 0) {
+            int size = q.size();
+            ArrayList<Integer> level = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode cur = q.poll();
+                if (cur.left != null)
+                    q.offer(cur.left);
+                if (cur.right != null)
+                    q.offer(cur.right);
+                level.add(cur.val);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
