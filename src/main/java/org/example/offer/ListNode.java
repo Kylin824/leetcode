5fbbaddd72db68c3d0649a51f750bcc0ff198104
linkangying <linkangying@huya.com>
2020-07-06 16:35:45 +0800

    add files

--- src/main/java/org/example/offer/ListNode.java
+++ src/main/java/org/example/offer/ListNode.java
@@ -7,4 +7,12 @@ class ListNode {
     ListNode(int val) {
         this.val = val;
     }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(0);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = head.next;
+    }
 }
