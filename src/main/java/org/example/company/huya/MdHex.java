e58b65a7fc28063e70efc0f9cb199e1d7459740c
linkangying <linkangying@huya.com>
2020-07-22 10:01:35 +0800

    add code

---
+++ src/main/java/org/example/company/huya/MdHex.java
@@ -0,0 +1,12 @@
+package org.example.company.huya;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class MdHex {
+    private final static String KEY_PATTERN = "pitaya_%s";
+
+    public static void main(String[] args) {
+        String key = String.format(KEY_PATTERN, DigestUtils.md5Hex("00010812-B043-4E17-837D-8D8E568CF0A8"));
+        System.out.println(key);
+    }
+}
