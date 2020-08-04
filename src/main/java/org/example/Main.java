c67087d6f3a1199512412b1de946255b8fdd3f8c
linkangying <linkangying@huya.com>
2020-08-04 17:18:42 +0800

    add code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,5 +15,21 @@ public class Main {
 
         System.out.println(5/2);
         System.out.println((float) 5/2);
+        String dt = "2020-08-08";
+        String sql1 = "select t1.install_channel, t1.mid, t2.oaid, t2.imei, t2.mac, t1.os, t1.machine, t2.ip, t2.atime,t1.product \n" +
+                "from \n" +
+                "(\n" +
+                "SELECT mid,os,machine,insdttall_channel,product\n" +
+                "FROM gamelive.dw_huya_new_user  \n" +
+                "WHERE dt='" + dt + "' AND new_user_type='ukey' AND  new_data_type='product' AND " +
+                //"(install_channel like 'a\\_%' or install_channel like '%baidusem%')\n" +
+                "(install_channel = 'vivo')\n" +
+                ") t1 \n" +
+                "left join \n" +
+                "(\n" +
+                "select mid, imei, mac, ip, oaid, min(time) as atime from user_growth.dim_action_detail where dt='"+ dt +"' group by " +
+                "mid, imei, mac, ip, oaid \n" +
+                ") t2 on (t1.mid=t2.mid) WHERE t2.mid IS NOT NULL";
+        System.out.println(sql1);
     }
 }
---
+++ src/main/java/org/example/offer/tree/LowestCommonAncestor.java
@@ -0,0 +1,50 @@
+package org.example.offer.tree;
+
+import org.example.offer.TreeNode;
+
+public class LowestCommonAncestor {
+    /*
+        NO.68 二叉树的公共祖先
+     */
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q); // 后序
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null && right == null)
+            return null;
+        if (left == null)
+            return right;
+        if (right == null)
+            return left;
+        return root;
+
+    }
+
+    public static void main(String[] args) {
+        /*
+                         5
+                       /  \
+                      8    6
+                     / \  / \
+                    9  2 1   0
+                      / \
+                     4  7
+        */
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(8);
+        root.left.left = new TreeNode(9);
+        TreeNode p = root.left.left; // 9
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(4);
+        root.left.right.right = new TreeNode(7);
+        TreeNode q = root.left.right.right; // 7
+        root.right = new TreeNode(6);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(0);
+
+        TreeNode commonAncestor = lowestCommonAncestor(root, p, q);
+        System.out.println(commonAncestor.val);
+    }
+}
