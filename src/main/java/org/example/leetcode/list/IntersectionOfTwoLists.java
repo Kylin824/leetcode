68609b3e75f11d0646ddb28e9313b606bba5d56c
CalvinLin <31285778+Calvin24lky@users.noreply.github.com>
2020-08-22 14:07:15 +0800

    add code

--- src/main/java/org/example/leetcode/list/IntersectionOfTwoLists.java
+++ src/main/java/org/example/leetcode/list/IntersectionOfTwoLists.java
@@ -2,13 +2,51 @@ package org.example.leetcode.list;
 
 import org.example.leetcode.ListNode;
 
+import java.util.jar.JarEntry;
+
 public class IntersectionOfTwoLists {
     /*
     160. 相交链表
     编写一个程序，找到两个单链表相交的起始节点。
      */
 
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != null && p2 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        if (p1 == null) {
+            p1 = headB;
+        }
+        else {
+            p2 = headA;
+        }
+        while (p1 != null && p2 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        if (p1 == null) {
+            p1 = headB;
+        }
+        else {
+            p2 = headA;
+        }
+        while (p1 != null && p2 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p1 == p2 ? p1 : null;
+    }
+
+    public static void main(String[] args) {
+        ListNode p1 = new ListNode(2);
+        p1.next = new ListNode(3);
+        ListNode p2 = p1.next;
+        ListNode ret = getIntersectionNode(p1, p2);
 
     }
 }
--- src/main/java/org/example/offer/list/FirstCommonNodeOfTwoList.java
+++ src/main/java/org/example/offer/list/FirstCommonNodeOfTwoList.java
@@ -14,38 +14,35 @@ public class FirstCommonNodeOfTwoList {
             第二遍使两个指针都从短链表开始往后找
             第三遍找到共同节点
      */
-    public static ListNode findFirstCommonNode(ListNode pHead1, ListNode pHead2) {
-        if (pHead1 == null || pHead2 == null)
+    public static ListNode findFirstCommonNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
             return null;
-        ListNode p1 = pHead1;
-        ListNode p2 = pHead2;
-
-        // 第一次遍历让长链表上的指针走短链表的长度
-        while (p1.next != null && p2.next != null) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != null && p2 != null) {
             p1 = p1.next;
             p2 = p2.next;
         }
-        // 短链表指针走到最后
-        if (p1.next == null)
-            p1 = pHead2;
-        else
-            p2 = pHead1;
-
-        // 第二次遍历让长链表上的指针走到和短链表指针起始的位置
-        while (p1.next != null && p2.next != null) {
+        if (p1 == null) {
+            p1 = headB;
+        }
+        else {
+            p2 = headA;
+        }
+        while (p1 != null && p2 != null) {
             p1 = p1.next;
             p2 = p2.next;
         }
-        if (p1.next == null)
-            p1 = pHead2;
-        else
-            p2 = pHead1;
-        // 此时p1,p2都从短的链表位置开始往后走
-        while (p1 != p2 && p1.next != null && p2.next != null) {
+        if (p1 == null) {
+            p1 = headB;
+        }
+        else {
+            p2 = headA;
+        }
+        while (p1 != null && p2 != null) {
             p1 = p1.next;
             p2 = p2.next;
         }
-
         return p1 == p2 ? p1 : null;
     }
 
