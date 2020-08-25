daa9b73f6fbf542a24c6d89ea080d47bd97a1e7c
linkangying <linkangying@huya.com>
2020-08-25 17:16:17 +0800

    add code

--- src/main/java/org/example/learn/concurrent/lock/LockDemo.java
+++ src/main/java/org/example/learn/concurrent/lock/LockDemo.java
@@ -5,11 +5,11 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class LockDemo {
     public static void main(String[] args) {
+
         Lock lock = new ReentrantLock();
         lock.lock();
-
         try {
-
+            // 获取锁后执行操作
         }
         // 在finally块中释放锁，目的是保证在获取到锁之后，最终能够被释放。
         finally {
