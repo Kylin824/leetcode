2880ccc9e3f82ebbc6149f47db45b4e7de1c549f
linkangying <linkangying@huya.com>
2020-08-26 10:43:37 +0800

    bug fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,7 @@
 package org.example;
+
 import java.util.*;
 public class Main {
     public static void main(String[] args){
-        int[] arr = {100, 5, 77, 4, 88, 6, 7, 8, 99, 33};
-        HashSet<Integer> set = new HashSet<Integer>();
-        for (int i = 0; i < arr.length; i++) {
-            set.add(arr[i]);
-        }
-        int max = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int count = 1;
-            int num = arr[i] - 1;
-            while (set.contains(num)) {
-                count++;
-                num--;
-            }
-            num = arr[i] + 1;
-            while (set.contains(num)) {
-                count++;
-                num++;
-            }
-            max = Math.max(max, count);
-        }
-        System.out.println(max);
     }
 }
