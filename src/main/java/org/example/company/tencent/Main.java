44f75b195ebfbb3f90ef96fb6db1e3ba8174884b
CalvinLin <31285778+Calvin24lky@users.noreply.github.com>
2020-09-07 09:38:59 +0800

    add codes

---
+++ src/main/java/org/example/company/tencent/Main.java
@@ -0,0 +1,12 @@
+package org.example.company.tencent;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = new int[5][];
+        int[] arr1 = {1, 2, 3, 4, 5};
+        arr[0] = arr1;
+        int[] arr2 = {1, 2, 3};
+        arr[1] = arr2;
+        System.out.println("test");
+    }
+}
