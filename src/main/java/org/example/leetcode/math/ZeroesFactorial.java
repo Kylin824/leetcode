24befffe736a01bf693690f0c4982c9150870ed4
CalvinLin <31285778+Calvin24lky@users.noreply.github.com>
2020-09-14 20:27:04 +0800

    add codes

--- src/main/java/org/example/leetcode/math/ZeroesFactorial.java
+++ src/main/java/org/example/leetcode/math/ZeroesFactorial.java
@@ -4,9 +4,16 @@ public class ZeroesFactorial {
     /*
     172. 阶乘后的零
     给定一个整数 n，返回 n! 结果尾数中零的数量。
+
+    零的数量跟5的数量有关
      */
 
     public int trailingZeroes(int n) {
-        return 0;
+        int count = 0;
+        while (n > 0) {
+            count += n / 5;
+            n = n / 5;
+        }
+        return count;
     }
 }
