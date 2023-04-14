f9449ec10ddd47bec23b61aa44b07b059d77c148
linkanging <linkanging@bigo.com>
2023-04-15 01:18:14 +0800

    feat:递归

--- src/main/java/org/example/hot100/tree/_617.java
+++ src/main/java/org/example/hot100/tree/_617.java
@@ -1,5 +1,6 @@
 package org.example.hot100.tree;
 
+import org.apache.spark.sql.sources.In;
 import org.example.TreeNode;
 import scala.Int;
 
@@ -20,103 +21,34 @@ public class _617 {
         _617 o = new _617();
 
         TreeNode root1 = new TreeNode(1);
-        root1.left = new TreeNode(2);
-        root1.left.left = new TreeNode(3);
+        root1.right = new TreeNode(1);
+        root1.right.left = new TreeNode(2);
 
         TreeNode root2 = new TreeNode(1);
-        root2.right = new TreeNode(2);
-        root2.right.right = new TreeNode(3);
+        root2.right = new TreeNode(1);
+        root2.right.right = new TreeNode(1);
+        root2.right.right.right = new TreeNode(1);
+        root2.right.right.right.left = new TreeNode(2);
 
-        System.out.println(o.depth(root1));
-        System.out.println(o.depth(root2));
-
-        o.levelOrderPrint(root1, 3);
-        System.out.println();
-        o.levelOrderPrint(root2,3);
+        TreeNode treeNode = o.mergeTrees(root1, root2);
+        System.out.println(treeNode);
     }
+
 
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        List<TreeNode> tree1 = new ArrayList<>();
-        List<TreeNode> tree2 = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-
-        List<TreeNode> res = new ArrayList<>();
-        int len = Math.max(tree1.size(), tree2.size());
-        for (int i = 0; i < len; i++) {
-            TreeNode n1 = null, n2 = null;
-            if (i < tree1.size() && i < tree2.size()) {
-                n1 = tree1.get(i);
-                n2 = tree2.get(i);
-            }
-            if (i > tree1.size()) {
-                n2 = tree2.get(i);
-            }
-            if (i > tree2.size()) {
-                n1 = tree1.get(i);
-            }
-            TreeNode node;
-            if (n1 == null && n2 == null) {
-                res.add(null);
-                continue;
-            }else if (n1 == null) {
-                node = new TreeNode(n2.val);
-            }else if (n2 == null) {
-                node = new TreeNode(n1.val);
-            }else {
-                node = new TreeNode(n1.val + n2.val);
-            }
-            res.add(node);
+        if (root1 == null) {
+            return root2;
         }
-
-        // 中序重建树
-        len = 1;
-        queue.clear();
-        queue.offer(res.get(0));
-        while (!queue.isEmpty()) {
-            int count = (int) (Math.log(len) / Math.log(2)) + 1;
-            while (count > 0) {
-                TreeNode node = queue.poll();
-                if (node != null) {
-                    if (len < res.size()) {
-                        node.left = res.get(len++);
-                        queue.offer(node.left);
-                    }
-                    if (len < res.size()) {
-                        node.right = res.get(len++);
-                        queue.offer(node.right);
-                    }
-                }
-                count--;
-            }
+        if (root2 == null) {
+            return root1;
         }
-
-        System.out.println();
-
-        queue.offer(res.get(0));
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size > 0) {
-                TreeNode node = queue.poll();
-                size--;
-                if (node == null) {
-                    System.out.print("null ");
-                    continue;
-                }
-                queue.offer(node.left);
-                queue.offer(node.right);
-                System.out.print(node.val + " ");
-            }
-        }
-
-        return res.get(0);
+        TreeNode merge = new TreeNode(root1.val + root2.val);
+        merge.left = mergeTrees(root1.left, root2.left);
+        merge.right = mergeTrees(root1.right, root2.right);
+        return merge;
     }
 
-    public int depth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return Math.max(depth(root.left), depth(root.right)) + 1;
-    }
+
 
     public void levelOrderTree(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
@@ -136,7 +68,6 @@ public class _617 {
             }
         }
     }
-
 
     public void levelOrderPrint(TreeNode root, int depth) {
         Queue<TreeNode> queue = new LinkedList<>();
@@ -163,16 +94,80 @@ public class _617 {
         }
     }
 
+
+    // 树高度
+    public int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+
+    // 树转层序列表
+    public List<Integer> treeToList(TreeNode root, int depth) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> res = new ArrayList<>();
+        queue.offer(root);
+        int curDepth = 0;
+        while (!queue.isEmpty()) {
+            curDepth++;
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                size--;
+                if (node == null) {
+                    if (curDepth <= depth) {
+                        queue.offer(null);
+                        queue.offer(null);
+                        res.add(null);
+                    }
+                    continue;
+                }
+                queue.offer(node.left);
+                queue.offer(node.right);
+                res.add(node.val);
+            }
+        }
+        return res;
+    }
+
     // 层序列表重建树
-    public TreeNode inorderToTree(List<Integer> list) {
-        int depth = 0;
+    public TreeNode listToTree(List<Integer> list) {
         Queue<TreeNode> queue = new LinkedList<>();
         TreeNode root = new TreeNode(list.get(0));
         queue.offer(root);
+        int count = 1;
+        while (!queue.isEmpty() && count < list.size()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                size--;
+                if (node == null) {
+                    count += 2;
+                    queue.offer(null);
+                    queue.offer(null);
+                    continue;
+                }
+                Integer val = list.get(count);
+                if (val != null) {
+                    node.left = new TreeNode(val);
+                } else {
+                    node.left = null;
+                }
+                queue.offer(node.left);
+                count++;
 
-        while (!queue.isEmpty()) {
-
+                val = list.get(count);
+                if (val != null) {
+                    node.right = new TreeNode(val);
+                } else {
+                    node.right = null;
+                }
+                queue.offer(node.right);
+                count++;
+            }
         }
+        return root;
     }
 
 }
