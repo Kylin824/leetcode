74ae7a42ca692b631456e00e3648f79336358419
linkanging <linkanging@bigo.com>
2023-04-16 19:18:36 +0800

    feat: dp substring

--- src/main/java/org/example/hot100/sub_sequence/_128.java
+++ src/main/java/org/example/hot100/sub_sequence/_128.java
@@ -25,41 +25,38 @@ public class _128 {
     public static void main(String[] args) {
         _128 o = new _128();
 
-        System.out.println(o.longestConsecutive1(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
-        System.out.println(o.longestConsecutive2(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
-        System.out.println(o.longestConsecutive1(new int[]{1, 2, 0, 1, 4, 5}));
-        System.out.println(o.longestConsecutive2(new int[]{1, 2, 0, 1, 4, 5}));
+//        System.out.println(o.longestConsecutive1(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
+//        System.out.println(o.longestConsecutive2(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
+//        System.out.println(o.longestConsecutive1(new int[]{1, 2, 0, 1, 4, 5}));
+//        System.out.println(o.longestConsecutive2(new int[]{1, 2, 0, 1, 4, 5}));
+//        System.out.println(o.longestConsecutive4(new int[]{100,4,200,1,3,2}));
+        System.out.println(o.longestConsecutive4(new int[]{4,1,3,2}));
 
     }
 
-    // 排序后遍历  O(nLogn)
+    // 排序后遍历  时间 O(nLogn) 空间O(1)
     public int longestConsecutive1(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
         Arrays.sort(nums);
-        int max = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int j = i;
-            int count = 1;
-            while (j + 1 < nums.length) {
-                if (nums[j + 1] == nums[j]) {
-                    j++;
-                } else if (nums[j + 1] == nums[j] + 1) {
-                    count++;
-                    j++;
-                } else {
-                    i = j;
-                    break;
-                }
+        int res = 1;
+        int len = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
             }
-            max = Math.max(max, count);
-            // 遍历的时候  如果下一个满足条件直接跳过
-            while (i + 1 < nums.length && (nums[i] == nums[i + 1] || nums[i] + 1 == nums[i + 1])) {
-                i++;
+            if (nums[i] == nums[i - 1] + 1) {
+                len++;
+                res = Math.max(res, len);
+            } else {
+                len = 1;
             }
         }
-        return max;
+        return res;
     }
 
-    // Set O(n)
+    // Set 时间 O(n) 空间O(1) 官解
     public int longestConsecutive2(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
@@ -78,27 +75,30 @@ public class _128 {
         return max;
     }
 
-    // HashMap 记录右边界 感觉不如set直接 两者速度差不多 这个空间消耗更多
-    public int longestConsecutive3(int[] nums) {
+    // dp
+    // https://leetcode.cn/problems/longest-consecutive-sequence/solution/dong-tai-gui-hua-python-ti-jie-by-jalan/
+    public int longestConsecutive4(int[] nums) {
+        // key表示num，value表示num所在连续区间的长度
         Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
         for (int num : nums) {
-            map.put(num, num);
-        }
-        int max = 0;
-        for (int num : nums) {
-            if (!map.containsKey(num - 1)) {
-                int right = map.get(num);
-                while (map.containsKey(right + 1)) {
-                    right = map.get(right + 1);
-                }
-                map.put(num, right);
-                max = Math.max(max, right - num + 1);
+            // 当map中不包含num，也就是num第一次出现
+            if (!map.containsKey(num)) {
+                // left为num-1所在连续区间的长度，更进一步理解为：左连续区间的长度
+                int left = map.getOrDefault(num - 1, 0);
+                // right为num+1所在连续区间的长度，更进一步理解为：右连续区间的长度
+                int right = map.getOrDefault(num + 1, 0);
+                // 当前连续区间的总长度
+                int curLen = left + right + 1;
+                ans = Math.max(ans, curLen);
+                // 将num加入map中，表示已经遍历过该值。其对应的value可以为任意值。
+                map.put(num, -1);
+                // 更新当前连续区间左边界和右边界对应的区间长度
+                map.put(num - left, curLen);
+                map.put(num + right, curLen);
             }
         }
-        return max;
+        return ans;
     }
-
-    // dp
-
 
 }
