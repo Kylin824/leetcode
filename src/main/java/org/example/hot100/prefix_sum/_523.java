09bd4f217f7e6f78be3971cf2898b55823f6b9a5
linkangying <linkangying@bigo.sg>
2023-05-23 20:13:34 +0800

    feat:前缀和

--- src/main/java/org/example/hot100/prefix_sum/_523.java
+++ src/main/java/org/example/hot100/prefix_sum/_523.java
@@ -1,8 +1,39 @@
 package org.example.hot100.prefix_sum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author kylin
  * @date 2023/5/23
  */
 public class _523 {
+
+    /**
+     * 给你一个整数数组 nums 和一个整数 k ，编写一个函数来判断该数组是否含有同时满足下述条件的连续子数组：
+     *
+     * 子数组大小 至少为 2 ，且
+     * 子数组元素总和为 k 的倍数。
+     * 如果存在，返回 true ；否则，返回 false 。
+     *
+     * 如果存在一个整数 n ，令整数 x 符合 x = n * k ，则称 x 是 k 的一个倍数。0 始终视为 k 的一个倍数。
+     */
+
+    public boolean checkSubarraySum(int[] nums, int k) {
+        if (nums.length == 0) {
+            return false;
+        }
+        int[] prenums = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prenums[i + 1] = prenums[i] + nums[i];
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i - 1; j++) {
+                if ((prenums[i] - prenums[j]) % k == 0) {
+
+                }
+            }
+        }
+    }
 }
--- src/main/java/org/example/hot100/prefix_sum/_724.java
+++ src/main/java/org/example/hot100/prefix_sum/_724.java
@@ -6,11 +6,34 @@ package org.example.hot100.prefix_sum;
  */
 public class _724 {
     /**
-     *  给你一个整数数组 nums ，请计算数组的 中心下标 。
+     * 给你一个整数数组 nums ，请计算数组的 中心下标 。
+     * <p>
      * 数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+     * <p>
+     * 如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+     * <p>
+     * 如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
      */
 
-    public int pivotIndex(int[] nums) {
-        return 0;
+    public static void main(String[] args) {
+        System.out.println(pivotIndex(new int[]{1, 7, 3, 6, 5, 6})); // 3
+        System.out.println(pivotIndex(new int[]{1, 2, 3})); // -1
+        System.out.println(pivotIndex(new int[]{2, 1, -1})); // 0
+    }
+
+    public static int pivotIndex(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int[] prenums = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prenums[i + 1] = prenums[i] + nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (prenums[i] == prenums[prenums.length - 1] - prenums[i + 1]) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
