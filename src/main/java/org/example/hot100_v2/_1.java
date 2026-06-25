62775dd86b35b8f9be8ec863ca451ac812a4901a
linkangying <linkangying@joyy.com>
2026-06-25 15:02:25 +0800

    feat: hot100

--- src/main/java/org/example/hot100_v2/_1.java
+++ src/main/java/org/example/hot100_v2/_1.java
@@ -1,8 +1,23 @@
 package org.example.hot100_v2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author kylin
  * @date 2026/6/25
  */
-public class _1 {
+public class _1 { // 两数之和
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n1 = nums[i];
+            Integer n2 = map.get(target - n1);
+            map.put(n1, i);
+            if (n2 != null && n2 != i) {
+                return new int[]{n2, i};
+            }
+        }
+        return new int[]{};
+    }
 }
