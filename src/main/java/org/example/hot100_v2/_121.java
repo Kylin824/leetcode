b10f72b058cf2e9d56e5523052f39ffaaa3913c7
linkangying <linkangying@joyy.com>
2026-06-29 18:48:04 +0800

    feat: hot100

---
+++ src/main/java/org/example/hot100_v2/_121.java
@@ -0,0 +1,15 @@
+package org.example.hot100_v2;
+
+/**
+ * 买卖股票最佳时机
+ *
+ * @author kylin
+ * @date 2026/6/29
+ */
+public class _121 {
+
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+
+}
