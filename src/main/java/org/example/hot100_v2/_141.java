dda909d4f60f9d9b4430728c424d14a95ea97e7a
kylin <493748267@qq.com>
2026-06-29 23:46:45 +0800

    feat: 141

---
+++ src/main/java/org/example/hot100_v2/_141.java
@@ -0,0 +1,20 @@
+package org.example.hot100_v2;
+
+import org.example.ListNode;
+
+public class _141 {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
