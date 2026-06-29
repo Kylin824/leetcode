a93118e660ef12ec95499c92c10beb91a5d6632b
kylin <493748267@qq.com>
2026-06-30 01:14:02 +0800

    feat: 160

---
+++ src/main/java/org/example/hot100_v2/_160.java
@@ -0,0 +1,49 @@
+package org.example.hot100_v2;
+
+import org.example.ListNode;
+
+public class _160 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1.next != null && p2.next != null) {
+            if (p1 == p2) {
+                return p1;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        if (p1.next == null) {
+            p1 = headB;
+            while (p2.next != null) {
+                p2 = p2.next;
+                p1 = p1.next;
+            }
+            p2 = headA;
+            while (p2 != null) {
+                if (p1 == p2) {
+                    return p2;
+                }
+                p2 = p2.next;
+                p1 = p1.next;
+            }
+        } else {
+            p2 = headA;
+            while (p1.next != null) {
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+            p1 = headB;
+            while (p1 != null) {
+                if (p1 == p2) {
+                    return p2;
+                }
+                p2 = p2.next;
+                p1 = p1.next;
+            }
+        }
+        return null;
+    }
+
+}
