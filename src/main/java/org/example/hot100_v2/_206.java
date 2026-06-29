4b4d686599e1e27545334de234b182e083553f48
kylin <493748267@qq.com>
2026-06-30 02:08:38 +0800

    feat: 206

---
+++ src/main/java/org/example/hot100_v2/_206.java
@@ -0,0 +1,34 @@
+package org.example.hot100_v2;
+
+import org.example.ListNode;
+
+import java.util.Stack;
+
+public class _206 {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = head.next;
+        ListNode sHead = reverseList(head.next);
+        head.next = null;
+        p.next = head;
+        return sHead;
+    }
+
+    public ListNode reverseListStack(ListNode head) {
+        if (head == null || head.next == null) return head;
+        Stack<ListNode> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+        head = stack.pop();
+        ListNode tail = head;
+        while (!stack.isEmpty()) {
+            tail.next = stack.pop();
+            tail = tail.next;
+        }
+        tail.next = null;
+        return head;
+    }
+}
