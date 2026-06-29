8d0d1603f5ce98c82be0f433a48be3ff890d27f5
linkangying <linkangying@joyy.com>
2026-06-29 15:56:12 +0800

    feat: hot100

---
+++ src/main/java/org/example/hot100_v2/_70.java
@@ -0,0 +1,25 @@
+package org.example.hot100_v2;
+
+/**
+ * 爬楼梯
+ *
+ * @author kylin
+ * @date 2026/6/26
+ */
+public class _70 {
+    public int climbStairs(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int[] res = new int[n + 1];
+        res[0] = 0;
+        res[1] = 1; // 1
+        res[2] = 2; // 1+1、2
+//        res[3] = 3; // 1+1+1、1+2、2+1
+//        res[4] = 5; // 1+1+1+1、1+2+1、2+1+1、1+1+2、2+2
+        for (int i = 3; i <= n; i++) {
+            res[i] = res[i - 1] + res[i - 2];
+        }
+        return res[n];
+    }
+}
---
+++ src/main/java/org/example/hot100_v2/_94.java
@@ -0,0 +1,60 @@
+package org.example.hot100_v2;
+
+import org.example.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 二叉树中序遍历
+ * @author kylin
+ * @date 2026/6/29
+ */
+public class _94 {
+
+    /**
+     * ps：[1,null,2,3]
+     *
+     *      1
+     *       \
+     *        2
+     *       /
+     *      3
+     *
+     * 前序遍历：打印 - 左 - 右 ：[1,2,3]
+     * 中序遍历：左 - 打印 - 右 ：[1,3,2]
+     * 后序遍历：左 - 右 - 打印 ：[3,2,1]
+     */
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(res, root);
+        return res;
+    }
+
+    public void inorder(List<Integer> res, TreeNode p) {
+        if (p == null) {
+            return;
+        }
+        inorder(res, p.left);
+        res.add(p.val);
+        inorder(res, p.right);
+    }
+
+
+    public List<Integer> inorderTraversalStack(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while(root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
