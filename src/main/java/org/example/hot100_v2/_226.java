c01be2e50b6db2a37ee5541da74afdf0c673f820
linkangying <linkangying@joyy.com>
2026-06-30 16:10:53 +0800

    feat: 226

---
+++ src/main/java/org/example/hot100_v2/_226.java
@@ -0,0 +1,25 @@
+package org.example.hot100_v2;
+
+import org.example.ListNode;
+import org.example.TreeNode;
+
+/**
+ * 翻转二叉树
+ *
+ * @author kylin
+ * @date 2026/6/30
+ */
+public class _226 {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.right = left;
+        root.left = right;
+        return root;
+    }
+
+}
