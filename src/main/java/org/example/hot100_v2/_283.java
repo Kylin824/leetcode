5d5ecebd9fc504f38e2528a02836fe61e1a68add
linkangying <linkangying@joyy.com>
2026-06-30 18:26:37 +0800

    feat: 283

---
+++ src/main/java/org/example/hot100_v2/_283.java
@@ -0,0 +1,35 @@
+package org.example.hot100_v2;
+
+import java.util.Arrays;
+
+/**
+ * @author kylin
+ * @date 2026/6/30
+ */
+public class _283 {
+    public static void moveZeroes(int[] nums) {
+        int p = 0;
+        while (p < nums.length) {
+            if (nums[p] == 0) {
+                int q = p + 1;
+                while (q < nums.length) {
+                    if (nums[q] != 0) {
+                        // 交换位置
+                        int tmp = nums[q];
+                        nums[q] = nums[p];
+                        nums[p] = tmp;
+                        break;
+                    } else {
+                        q++;
+                    }
+                }
+            }
+            p++;
+        }
+        System.out.println(Arrays.asList(nums));
+    }
+
+    public static void main(String[] args) {
+        moveZeroes(new int[]{0, 1, 0, 3, 12});
+    }
+}
