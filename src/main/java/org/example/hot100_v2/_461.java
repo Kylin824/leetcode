4ccb082fbe9d5d5b519a5d5fe3c468177b458935
linkangying <linkangying@joyy.com>
2026-06-30 20:36:35 +0800

    feat: 461

---
+++ src/main/java/org/example/hot100_v2/_461.java
@@ -0,0 +1,27 @@
+package org.example.hot100_v2;
+
+/**
+ * 汉明距离
+ * @author kylin
+ * @date 2026/6/30
+ */
+public class _461 {
+
+    public static int hammingDistance(int x, int y) {
+        int n = 0;
+        while (x != 0 || y != 0) {
+            int x1 = x & 1;
+            int y1 = y & 1;
+            if (x1 != y1) {
+                n++;
+            }
+            x = x >> 1;
+            y = y >> 1;
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hammingDistance(3, 1));
+    }
+}
