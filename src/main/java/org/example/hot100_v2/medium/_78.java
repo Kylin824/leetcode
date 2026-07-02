810657f3e6fd0974af667ef6c7a19903bd7683e8
kylin <493748267@qq.com>
2026-07-02 21:00:43 +0800

    feat: 78

--- src/main/java/org/example/hot100_v2/medium/_78.java
+++ src/main/java/org/example/hot100_v2/medium/_78.java
@@ -10,12 +10,28 @@ import java.util.List;
  */
 public class _78 {
 
-    public List<List<Integer>> subsets(int[] nums) {
+    public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-
-
-
+        List<Integer> path = new ArrayList<>();
+        int index = 0;
+        backTrack(nums, res, index, path);
         return res;
+    }
+
+    public static void backTrack(int[] nums, List<List<Integer>> res, int index, List<Integer> path) {
+        res.add(new ArrayList<>(path));
+
+        for (int i = index; i < nums.length; i++) {
+            path.add(nums[i]);
+            backTrack(nums, res, i + 1, path);
+            path.remove(path.size() - 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> subsets = subsets(new int[]{1, 2, 3}); // [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
+        System.out.println(subsets);
     }
 
 }
