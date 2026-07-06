1b163bbfa7708eb89c405a08b5616c55dae00880
linkangying <linkangying@bigo.sg>
2026-07-06 14:50:42 +0800

    feat: 739

--- src/main/java/org/example/hot100_v2/medium/_739.java
+++ src/main/java/org/example/hot100_v2/medium/_739.java
@@ -1,6 +1,7 @@
 package org.example.hot100_v2.medium;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * @author kylin
@@ -28,11 +29,25 @@ public class _739 {
         return res;
     }
 
+    public static int[] dailyTemperatures1(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (int i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
 //        int[] ints = dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73});
 //        int[] ints = dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73});
-        int[] ints = dailyTemperatures(new int[]{89,62,70,58,47,47,46,76,100,70});
-        System.out.println(Arrays.toString(ints));
+        System.out.println(Arrays.toString(dailyTemperatures(new int[]{89, 62, 70, 58, 47, 47, 46, 76, 100, 70})));
+        System.out.println(Arrays.toString(dailyTemperatures1(new int[]{89, 62, 70, 58, 47, 47, 46, 76, 100, 70})));
     }
 
 }
