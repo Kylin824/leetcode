1297afb461cdf8a0fe96556b5f5860b8215be82c
kylin <493748267@qq.com>
2026-07-09 02:24:23 +0800

    feat: 347

--- src/main/java/org/example/hot100_v2/easy/_121.java
+++ src/main/java/org/example/hot100_v2/easy/_121.java
@@ -12,8 +12,8 @@ public class _121 {
         int min = prices[0];
         int maxProfit = 0;
         for (int i = 1; i < prices.length; i++) {
-            min = Math.min(min, prices[i]);
-            maxProfit = Math.max(maxProfit, prices[i] - min);
+            min = Math.min(min, prices[i]); // 价格最低的一天
+            maxProfit = Math.max(maxProfit, prices[i] - min); // 当天卖 or 当天没有可买的
         }
         return maxProfit;
     }
