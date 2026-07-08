39d66726e14b1fe777b1c70aa793d3a726eb13da
linkangying <linkangying@joyy.com>
2026-07-08 20:41:26 +0800

    feat: 538

---
+++ src/main/java/org/example/hot100_v2/medium/tree/_538.java
@@ -0,0 +1,24 @@
+package org.example.hot100_v2.medium.tree;
+
+import org.example.TreeNode;
+
+/**
+ * 二叉搜索树转为累加树
+ * @author kylin
+ * @date 2026/7/8
+ */
+public class _538 {
+
+    public int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
+    }
+}
