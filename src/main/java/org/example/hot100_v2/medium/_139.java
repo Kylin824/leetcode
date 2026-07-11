1948a45e76cb5daae041a06aceb355a2792dbff8
kylin <493748267@qq.com>
2026-07-11 15:55:04 +0800

    feat: 139

--- src/main/java/org/example/hot100_v2/medium/_139.java
+++ src/main/java/org/example/hot100_v2/medium/_139.java
@@ -1,8 +1,6 @@
 package org.example.hot100_v2.medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author kylin
@@ -11,7 +9,35 @@ import java.util.List;
 
 public class _139 {
 
+    private Map<String, Boolean> memory = new HashMap<>();
+
     public boolean wordBreakReverse(String s, List<String> wordDict) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        if (memory.containsKey(s)) {
+            return memory.get(s);
+        }
+
+        List<String> prefixList = new ArrayList<>();
+        for (String sub : wordDict) {
+            if (s.startsWith(sub)) {
+                prefixList.add(sub);
+            }
+        }
+        if (!prefixList.isEmpty()) {
+            for (String prefix : prefixList) {
+                String subStr = s.substring(prefix.length());
+                if (wordBreakReverse(subStr, wordDict)) {
+                    memory.put(subStr, true);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
         if (s == null || s.length() == 0) {
             return true;
         }
@@ -33,9 +59,9 @@ public class _139 {
 
     public static void main(String[] args) {
         _139 obj = new _139();
-        System.out.println(obj.wordBreakReverse("leetcode", Arrays.asList("leet", "code")));
-        System.out.println(obj.wordBreakReverse("abcd", Arrays.asList("a","abc","b","cd")));
-        System.out.println(obj.wordBreakReverse("catsandog", Arrays.asList("cats","dog","sand","and","cat")));
+        System.out.println(obj.wordBreakReverse("leetcode", Arrays.asList("leet", "code"))); // true
+        System.out.println(obj.wordBreakReverse("abcd", Arrays.asList("a","abc","b","cd"))); // true
+        System.out.println(obj.wordBreakReverse("catsandog", Arrays.asList("cats","dog","sand","and","cat"))); // false
     }
 
 }
