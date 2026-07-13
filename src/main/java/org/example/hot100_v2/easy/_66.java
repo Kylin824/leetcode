9935c78c448de6e2d0bfcf9f3b9bb583e3945054
kylin <493748267@qq.com>
2026-07-14 00:35:22 +0800

    feat: 66

---
+++ src/main/java/org/example/hot100_v2/easy/_66.java
@@ -0,0 +1,38 @@
+package org.example.hot100_v2.easy;
+
+import java.util.Arrays;
+
+/**
+ * @author kylin
+ * @date 2026/7/14
+ */
+
+public class _66 {
+
+    public static int[] plusOne(int[] digits) {
+        int[] res = new int[digits.length + 1];
+        int carry = 0;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int num = digits[i] + carry;
+            if (i == digits.length - 1) {
+                num += 1;
+            }
+            if (num > 9) {
+                carry = 1;
+                res[i + 1] = num % 10;
+            } else {
+                carry = 0;
+                res[i + 1] = num;
+            }
+        }
+        if (carry > 0) {
+            res[0] = carry;
+        }
+        return carry == 1 ? res : Arrays.copyOfRange(res, 1, res.length);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(plusOne(new int[]{4,3,2,1})));
+        System.out.println(Arrays.toString(plusOne(new int[]{9,9,9})));
+    }
+}
